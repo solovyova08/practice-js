d62e4406edf81ffaeaf4b9d31dca989f2135e438
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-10-12 11:12:31 +0200

    feat: task 4,5

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -78,73 +78,127 @@
 
 // figure.addEventListener('click', setRandomState)
 
-// 3. Створити розмітку на основі масива даних, де у кожного елемента списку
-// буде вказано ім'я, кількість лайків і перелічені теги
-// в index.html додайте список ul.stats, в який буде рендеритись цей список
-// *властивість gender використай для додавання відповідного класу елементу списка
+// // 3. Створити розмітку на основі масива даних, де у кожного елемента списку
+// // буде вказано ім'я, кількість лайків і перелічені теги
+// // в index.html додайте список ul.stats, в який буде рендеритись цей список
+// // *властивість gender використай для додавання відповідного класу елементу списка
 
-const tweets = [
-    {
-        id: "000",
-        name: "Alex",
-        gender: "male",
-        likes: 5,
-        tags: ["js", "nodejs"],
-    },
-    {
-        id: "001",
-        name: "Kate",
-        gender: "female",
-        likes: 2,
-        tags: ["html", "css"],
-    },
-    {
-        id: "002",
-        name: "Maria",
-        gender: "female",
-        likes: 17,
-        tags: ["html", "js", "nodejs"],
-    },
-    {
-        id: "003",
-        name: "Borys",
-        gender: "male",
-        likes: 8,
-        tags: ["css", "react"],
-    },
-    {
-        id: "004",
-        name: "Jhon",
-        gender: "male",
-        likes: 10,
-        tags: ["js", "nodejs", "react"],
-    },
-    {
-        id: "005",
-        name: "Anna",
-        gender: "female",
-        likes: 3,
-        tags: ["js", "nodejs", "react"],
-    },
-    {
-        id: "006",
-        name: "Jhon",
-        gender: "male",
-        likes: 0,
-        tags: ["js", "nodejs", "react"],
-    },
-];
+// const tweets = [
+//     {
+//         id: "000",
+//         name: "Alex",
+//         gender: "male",
+//         likes: 5,
+//         tags: ["js", "nodejs"],
+//     },
+//     {
+//         id: "001",
+//         name: "Kate",
+//         gender: "female",
+//         likes: 2,
+//         tags: ["html", "css"],
+//     },
+//     {
+//         id: "002",
+//         name: "Maria",
+//         gender: "female",
+//         likes: 17,
+//         tags: ["html", "js", "nodejs"],
+//     },
+//     {
+//         id: "003",
+//         name: "Borys",
+//         gender: "male",
+//         likes: 8,
+//         tags: ["css", "react"],
+//     },
+//     {
+//         id: "004",
+//         name: "Jhon",
+//         gender: "male",
+//         likes: 10,
+//         tags: ["js", "nodejs", "react"],
+//     },
+//     {
+//         id: "005",
+//         name: "Anna",
+//         gender: "female",
+//         likes: 3,
+//         tags: ["js", "nodejs", "react"],
+//     },
+//     {
+//         id: "006",
+//         name: "Jhon",
+//         gender: "male",
+//         likes: 0,
+//         tags: ["js", "nodejs", "react"],
+//     },
+// ];
 
-// буде вказано ім'я, кількість лайків і перелічені теги
-// в index.html додайте список ul.stats, в який буде рендеритись цей список
-// *властивість gender використай для додавання відповідного класу елементу списка
+// // буде вказано ім'я, кількість лайків і перелічені теги
+// // в index.html додайте список ul.stats, в який буде рендеритись цей список
+// // *властивість gender використай для додавання відповідного класу елементу списка
 
-const stats = document.querySelector('.stats');
-const markup = tweets.reduce((html, tweet) => {
-    const li = `<li class="stats-item ${tweet.gender}"><p class='stats-name'>Name: ${tweet.name}</p><p class="stats-likes">Likes: ${tweet.likes}</p><p class="stats-tags">Tags: ${tweet.tags.join(', ')}</p></li>`;
-    html += li;
-    return html;
-}, '');
+// const stats = document.querySelector('.stats');
+// const markup = tweets.reduce((html, tweet) => {
+//     const li = `<li class="stats-item ${tweet.gender}"><p class='stats-name'>Name: ${tweet.name}</p><p class="stats-likes">Likes: ${tweet.likes}</p><p class="stats-tags">Tags: ${tweet.tags.join(', ')}</p></li>`;
+//     html += li;
+//     return html;
+// }, '');
 
-stats.innerHTML = markup;
+// stats.innerHTML = markup;
 
+
+// // 4. Кнопка "Приховати" ховає текст і замінює назву кнопки на
+// // "Розкрити", при повторному натисканні текст знову стає доступним
+// // і кнопка набуває початкового вигляду.
+
+// const input = document.querySelector("#passwordInput")
+// const btn = document.querySelector("#passwordButton")
+
+// btn.addEventListener("click", () => {
+//     if (btn.textContent === "Розкрити") {
+//         btn.textContent = "Приховати"
+//         input.style.color = "inherit"
+//     } else {
+//     btn.textContent = "Розкрити"
+//     input.style.color = "transparent"
+//     }
+
+// })
+
+
+// // 5. Натиснувши кнопку "Подвоювати", збільшити значення
+// // у кожному елементі списку у 2 рази
+
+// const btn = document.querySelector("#double")
+// const items = document.querySelectorAll(".listItem")
+
+// btn.addEventListener("click", () => {
+//     items.forEach(item => item.textContent *= 2)
+// })
+
+
+// 6. Видалити зі списку елементи, які позначені.
+const form = document.querySelector(".checkboxForm")
+const list = document.querySelector(".checkboxList")
+const items = document.querySelectorAll(".checkboxWrapper")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const newItems = []
+
+    items.forEach(item => {
+        if (!item.lastElementChild.checked) {
+            newItems.push(item)
+        }
+    })
+
+    list.innerHTML = ""
+    if (newItems.length === 0) {
+        list.innerHTML = "No more checkboxes now!"
+    } else {
+        list.append(...newItems)
+    }
+})
